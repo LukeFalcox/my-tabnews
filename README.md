c0d1cda53b1edc61067936f97323a036974707d9
LukeFalcox <101217187+LukeFalcox@users.noreply.github.com>
2023-12-01 15:09:01 +0000

    adciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # my-tabnews
+
 Implementação do curso do filipedeschamps https://www.tabnews.com.br para o https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,12 @@
 function Home() {
-<div>HOME</div>
+  <div>HOME</div>;
+}
+
+function teste() {
+  console.log("Teste");
+}
+
+function teste2(params) {
+  console.log("lindo");
 }
 export default Home;
